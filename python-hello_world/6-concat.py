a50680112331d0ec2ebd085ebded6f3117814150
adicto55 <your-email@example.com>
2026-01-04 21:09:12 +0400

    ds

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+str1 = "Welcome to "
+str2 = "Holberton School!"
+print(str1 + str2)
---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[51:82] + str[113:118] + str[:6])
