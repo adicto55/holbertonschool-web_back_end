6e0f11adb02698fbeb1f5b07d6625ebea3f461f4
Your Name <you@example.com>
2026-03-28 12:26:36 +0000

    SD

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
-"""
-Basic Flask application setup
-"""
+""" a basic flask app"""
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Renders a basic html template
-    """
+@app.route('/')
+def hello():
+    """ render a basic html file """
     return render_template('0-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run()
